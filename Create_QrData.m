b117f0c766e9e55d35aca344f3f8dbf15932d694
TzuWei-LIn <freelifetw29@gmail.com>
2015-05-04 14:43:35 +0800

    Creat QR code
    Version 1

---
+++ Create_QrData.m
@@ -0,0 +1,39 @@
+rd = importdata('Secert_Rand.mat');
+I = imread('Lena.jpg');
+
+% Devide block into martix x
+count =1;
+for i=1:4:size(I,1)
+    for j=1:4:size(I,2)
+        x(:,:,count) =I(i:i+3,j:j+3); %spilt origin image in x martix
+        count=count+1;
+    end
+end
+
+%clean LSB1 to 0 from matrix x
+for g=1:count-1
+    for i=1:4        for j=1:4
+            x1 = dec2bin(x(i,j,g))-48;
+            if(x1(length(x1))==1)
+                x(i,j,g) = x(i,j,g)-1;
+            end
+        end
+    end
+end
+
+% Generate Secert code (SVD) and store in matrix S1 and S2 and Claculate
+% Diff
+for i=1:size(x,3)
+    [U S V] = svd(double(x(:,:,i)));
+    S1(i) = fix(S(1,1));
+    S2(i) = fix(S(2,2));
+    
+    %     Calculate Diff and store in Secert_Diff_1 and Secert_Diff_2
+    if(i==1)
+        Secert_Diff_1(i) = S1(i);
+        Secert_Diff_2(i) = S2(i);
+    else
+        Secert_Diff_1(i) = S1(i) - S1(i-1);
+        Secert_Diff_2(i) = S2(i) - S2(i-1);
+    end
+end
