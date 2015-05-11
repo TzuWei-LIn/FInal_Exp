4eee8e63d1525ad76e7d7f1f06790cc089f0a234
TzuWei-LIn <freelifetw29@gmail.com>
2015-05-11 15:21:03 +0800

    Version3.2
    divid block 4x4 -> 8x8

--- Create_QrData.m
+++ Create_QrData.m
@@ -3,17 +3,18 @@ I = imread('Lena.jpg');
 
 % Devide block into martix x
 count =1;
-for i=1:4:size(I,1)
-    for j=1:4:size(I,2)
-        x(:,:,count) =I(i:i+3,j:j+3); %spilt origin image in x martix
+for i=1:8:size(I,1)
+    for j=1:8:size(I,2)
+        x(:,:,count) =I(i:i+7,j:j+7); %spilt origin image in x martix
         count=count+1;
     end
 end
 
 %clean LSB1 to 0 from matrix x
 for g=1:count-1
-    for i=1:4        for j=1:4
-            x1 = dec2bin(x(i,j,g))-48;
+    for i=1:8
+        for j=1:8
+            x1 = dec2bin(x(:,:,g))-48;
             if(x1(length(x1))==1)
                 x(i,j,g) = x(i,j,g)-1;
             end
